bb12ed44aa61e9f938b79109961da914b2f36c42
Elmehdi <eaitbrahim@gmail.com>
2020-02-25 13:17:48 +0100

    Create lambda websocket functions and DynamoTable

---
+++ lambdas/websockets/message.js
@@ -0,0 +1,28 @@
+const Responses = require('../common/API_Responses');
+const Dynamo = require('../common/Dynamo');
+
+const tableName = process.env.tableName;
+
+exports.handler = async event => {
+  console.log('event', event);
+
+  const { connectionId: connectionID } = event.requestContext;
+
+  const body = JSON.parse(event.body);
+
+  try {
+    const record = await Dynamo.get(connectionID, TableName);
+    const messages = record.messages;
+    messages.push(body.message);
+
+    const data = {
+      ...record,
+      messages
+    };
+
+    await Dynamo.write(data, tableName);
+    return Response._200({ message: 'got a message' });
+  } catch (error) {
+    return Responses._400({ message: 'message could not be received' });
+  }
+};
